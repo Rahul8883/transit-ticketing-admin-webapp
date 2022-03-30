8cd0e19ffdb6c6b88f8b76cdfca1e2cc23e5a69d
Rahul8883 <54974202+Rahul8883@users.noreply.github.com>
2022-03-30 12:37:59 +0530

    Nav impl

--- src/Components/Navbar/navbar.tsx
+++ src/Components/Navbar/navbar.tsx
@@ -1,81 +1,41 @@
 import {
+    Heading,
     Box,
+    Center,
     Flex,
     Text,
-    IconButton,
-    Button,
-    Stack,
-    Collapse,
-    Icon,
-    Link,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    useColorModeValue,
-    useBreakpointValue,
-    useDisclosure,
-  } from '@chakra-ui/react';
-  import {
-    HamburgerIcon,
-    CloseIcon,
-    ChevronDownIcon,
-    ChevronRightIcon,
-  } from '@chakra-ui/icons';
-  
-  export default function Navbar() {
-    const { isOpen, onToggle } = useDisclosure();
-  
+} from '@chakra-ui/react';
+import './navbar.css';
+import AngleBracket from '../../Assets/Vector.svg';
+import Pipe from '../../Assets/Pipe.svg';
+
+export default function Navbar() {
     return (
-      <Box 
-      width="100%"
-      display="flex"
-      justifyContent="flex-end"
-      >
-       <Flex
-          bg={useColorModeValue('white', 'gray.800')}
-          color={useColorModeValue('gray.600', 'white')}
-          minH={'270px'}
-          width="1237px"
-          py={{ base: 2 }}
-          px={{ base: 4 }}
-          borderBottom={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.900')}
-          align={'center'}>
-      
-        
-  
-          {/* <Stack
-        //   position="absolute"
-            // flex={{ base: 1, md: 60 }}
-            // justify={'flex-end'}
-            // direction={'row'}
-            // spacing={6}
+        <Center display={'flex'} justifyContent="end">
+            <Box
+                maxW={'1232px'}
+                h='354px'
+                w={'full'}
             >
-            <Button
-              as={'a'}
-              fontSize={'sm'}
-              fontWeight={400}
-              variant={'link'}
-              href={'#'}>
-              Sign In
-            </Button>
-            <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
-              fontWeight={600}
-              color={'white'}
-              bg={'pink.400'}
-            //   href={'#'}
-              _hover={{
-                bg: 'pink.300',
-              }}>
-              Sign Up
-            </Button>
-          </Stack> */}
-        </Flex>
-      </Box>
+                <Box className='Nav-heading'><Text>Reports</Text></Box>
+                <Flex justifyContent={'space-around'}>
+                    <Box className='Nav-container'>
+                        <Text className='Nav-content-head'>Total Reports</Text>
+                        <Text className='Nav-content-child'>100</Text>
+                    </Box>
+                    <Box><img src={AngleBracket} /></Box>
+                    <Box>
+                        <Text className='Nav-content-head'>Total Reports</Text>
+                        <Text className='Nav-content-child'>50</Text>
+                    </Box>
+                    <Box><img src={Pipe} /></Box>
+                    <Box>
+                        <Text className='Nav-content-head'>Total Reports</Text>
+                        <Text className='Nav-content-child'>50</Text>
+                    </Box>
+                </Flex>
+                <Box className='Table-heading'><Text>Way Bill Reports :</Text></Box>
+            </Box>
+        </Center>
     );
-  }
-  
- 
+}
--- src/Components/Navigation/navigation.tsx
+++ src/Components/Navigation/navigation.tsx
@@ -1,13 +1,9 @@
 import React, { ReactNode, ReactText } from "react";
 import {
-  IconButton,
   Avatar,
   Box,
   CloseButton,
   Flex,
-  HStack,
-  VStack,
-  Icon,
   useColorModeValue,
   Link,
   Drawer,
@@ -17,29 +13,8 @@ import {
   BoxProps,
   FlexProps,
   Divider,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  DrawerFooter,
 } from "@chakra-ui/react";
-import {
-  FiHome,
-  FiTrendingUp,
-  FiCompass,
-  FiStar,
-  FiSettings,
-  FiMenu,
-  FiBell,
-  FiChevronDown,
-} from "react-icons/fi";
 import { IconType } from "react-icons";
-import { useHistory } from "react-router-dom";
-import {useState, useEffect} from 'react';
-import logging from "../../Config/logging";
-import { auth } from "../../Config/firebase";
-import { deleteLocalStorage } from "../../utils/helpers";
 import logo from '../../Assets/governmentOfKerlaLogo.svg';
 import './navigation.css'
 import Navbar from '../Navbar/navbar';
@@ -48,17 +23,6 @@ interface LinkItemProps {
   name: string;
   icon: IconType;
 }
-// const LinkItems: Array<LinkItemProps> = [
-//   { name: "Home", icon: FiHome },
-//   { name: "Boats", icon: FiTrendingUp },
-//   { name: "Operational Boats", icon: FiCompass },
-//   { name: "Schedules", icon: FiStar },
-//   { name: "Staff", icon: FiSettings },
-//   { name: "Assignments", icon: FiSettings },
-//   { name: "Boats", icon: FiSettings },
-//   { name: "Counter", icon: FiSettings },
-
-// ];
 
 export default function Navigation({
   children,
@@ -130,11 +94,6 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
         </Text>
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
-      {/* {LinkItems.map((link) => (
-        <NavItem key={link.name} >
-          {link.name}
-        </NavItem>
-      ))} */}
       <NavItem className="main-drawer-item">Home</NavItem>
       <NavItem className="main-drawer-item">Boats</NavItem>
       <Divider/>
@@ -154,32 +113,20 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
       </div>
       </Flex>
       <Flex alignItems="end" h="28vh">
-        {/* <NavItem> */}
         <Avatar left="24px"
          size={"sm"}
                   src={parsedData.photoURL}
                 />
-        {/* </NavItem> */}
-
         <Text className="footer-User-name"
-        >{parsedData.displayName?parsedData.displayName : "Rahul"}</Text>
- 
+        >{parsedData.displayName?parsedData.displayName : parsedData.email}</Text>
       </Flex>
-     
-      {/* <NavItem>Home</NavItem>
-      <NavItem>Home</NavItem>
-      <NavItem>Home</NavItem>
-      <NavItem>Home</NavItem> */}
-      
     </Box>
   );
 };
 
 interface NavItemProps extends FlexProps {
-  // icon: IconType;
   children: ReactText;
 }
-//icon
 const NavItem = ({ children, ...rest }: NavItemProps) => {
   return (
     <Link href="#" style={{ textDecoration: "none", textAlign: "left" }} _focus={{ boxShadow: "none" }}>
@@ -195,111 +142,8 @@ const NavItem = ({ children, ...rest }: NavItemProps) => {
           color: "white",
         }}
         {...rest}>
-        {/* {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: "white",
-            }}
-            as={icon}
-          />
-        )} */}
         {children}
       </Flex>
     </Link>
   );
 };
-
-// interface MobileProps extends FlexProps {
-//   onOpen: () => void;
-// }
-// const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-//   const history = useHistory();
-
-//   const logout = () => {
-//     auth.signOut()
-//       .then(() => {
-//         deleteLocalStorage("firebaseData");
-//         history.push('/login');
-//       })
-//       .catch(error => logging.error(error));
-//   }
-
-//   return (
-//     <Flex
-//       ml={{ base: 0, md: 60 }}
-//       px={{ base: 4, md: 4 }}
-//       height="20"
-//       alignItems="center"
-//       bg={useColorModeValue("white", "gray.900")}
-//       borderBottomWidth="1px"
-//       borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-//       justifyContent={{ base: "space-between", md: "flex-end" }}
-//       {...rest}>
-//       <IconButton
-//         display={{ base: "flex", md: "none" }}
-//         onClick={onOpen}
-//         variant="outline"
-//         aria-label="open menu"
-//         icon={<FiMenu />}
-//       />
-
-//       <Text
-//         display={{ base: "flex", md: "none" }}
-//         fontSize="1xl"
-//         fontFamily="monospace"
-//         fontWeight="bold">
-//         State Water Transport Department
-//       </Text>
-
-//       <HStack spacing={{ base: "0", md: "6" }}>
-//         <IconButton
-//           size="lg"
-//           variant="ghost"
-//           aria-label="open menu"
-//           icon={<FiBell />}
-//         />
-//         <Flex alignItems={"center"}>
-//           <Menu>
-//             <MenuButton
-//               py={2}
-//               transition="all 0.3s"
-//               _focus={{ boxShadow: "none" }}>
-//               <HStack>
-//                 <Avatar
-//                   size={"sm"}
-//                   src={
-//                     "https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-//                   }
-//                 />
-//                 <VStack
-//                   display={{ base: "none", md: "flex" }}
-//                   alignItems="flex-start"
-//                   spacing="1px"
-//                   ml="2">
-//                   <Text fontSize="sm">Justina Clark</Text>
-//                   <Text fontSize="xs" color="gray.600">
-//                     Admin
-//                   </Text>
-//                 </VStack>
-//                 <Box display={{ base: "none", md: "flex" }}>
-//                   <FiChevronDown />
-//                 </Box>
-//               </HStack>
-//             </MenuButton>
-//             <MenuList
-//               bg={useColorModeValue("white", "gray.900")}
-//               borderColor={useColorModeValue("gray.200", "gray.700")}>
-//               <MenuItem>Profile</MenuItem>
-//               <MenuItem>Settings</MenuItem>
-//               <MenuItem>Billing</MenuItem>
-//               <MenuDivider />
-//               <MenuItem onClick={() => logout()}> Sign out</MenuItem>
-//             </MenuList>
-//           </Menu>
-//         </Flex>
-//       </HStack>
-//     </Flex>
-//   );
-// };
